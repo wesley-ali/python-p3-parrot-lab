a435de70b79ca2529bb26284ba61fe9e9938f3ee
Wesley Alinyo <wesleyalinyo@gmail.com>
2024-09-22 15:38:43 +0300

    Adds file

--- lib/parrot.py
+++ lib/parrot.py
@@ -1,2 +1,9 @@
-def parrot():
-    pass
+def parrot(message="Squawk!"):
+    print(message)
+    return message
+
+# Example;
+result = parrot("Hello!")
+print(result)  #It will print "Hello!" and return it
+
+    
